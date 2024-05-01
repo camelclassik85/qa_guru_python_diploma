83305edceb0118052d14e5c0dedfbaadb3e1e871
Alexander_Dulalaev <camel1985@mail.ru>
2024-05-01 18:41:14 +0300

    correct cookie add to browser

--- start/utils/cookie.py
+++ start/utils/cookie.py
@@ -1,10 +1,10 @@
-import logging
 from selene import browser
+from start.constants import web_base_url
 from start.data.users import User
 
 
 def add_auth_cookie_to_browser(user: User, url: str = '/'):
-    browser.open(url)
-    browser.driver.add_cookie({"name": "auth", "value": user.auth})
-    # logging.info(browser.driver.get_cookie('auth'))
-    browser.driver.refresh()
+    browser.config.base_url = ''
+    browser.open('https://api.start.ru/')
+    browser.driver.add_cookie({"name": "auth", "value": user.auth, "HttpOnly": True, "Secure": True, "SameSite": None})
+    browser.open(web_base_url + url)
